ca67e7f4e9796d123440f3d639fc8252e0aa19be
Melon Ask <255711987+melonask@users.noreply.github.com>
2026-09-16 18:06:07 +0300

    Update PingBar to 1.3.1

--- Casks/pingbar.rb
+++ Casks/pingbar.rb
@@ -1,6 +1,6 @@
 cask "pingbar" do
-  version "1.3.0"
-  sha256 "fe303b5f00dc3e787bfbd63b6f466997e5961d55b2d77ef69dd988f9e1829d4a"
+  version "1.3.1"
+  sha256 "e790f89ea2de336ea349c3d0edc45fd9c37d3307b0328c91bf044e3d5247efbe"
 
   url "https://github.com/melonask/PingBar/releases/download/v#{version}/PingBar.zip"
   name "PingBar"
@@ -17,6 +17,6 @@ cask "pingbar" do
   caveats <<~EOS
     PingBar is ad-hoc signed rather than notarized. If macOS blocks the first
     launch, Control-click PingBar.app in Applications and choose Open, or run:
-      xattr -dr com.apple.quarantine "#{appdir}/PingBar.app"
+      /usr/bin/xattr -dr com.apple.quarantine "#{appdir}/PingBar.app"
   EOS
 end
